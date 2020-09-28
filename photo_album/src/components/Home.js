24e1d2e7eb78cea101413c4f6b20aca5f7622cfc
gabriel <alang49@live.com>
2020-09-27 19:19:26 -0700

    Add changes to Home page

--- photo_album/src/components/Home.js
+++ photo_album/src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import memories from '../assets/img/memories.jpg';
 
 export default function Home() {
 
     return (
-        <>
-            <div>hOME</div>
-        </>
+            <div>
+                <div class="alert alert-primary text-center" role="alert">
+                 <b>Save all your memories here</b>
+                </div>
+                <img src={memories} class="img-fluid" alt="Responsive image" />
+            </div>
     );
 }
